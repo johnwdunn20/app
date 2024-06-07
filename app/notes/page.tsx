2b629fe7fd5b9f976eb2e37b8833040f83f44ed4
John Dunn <johnwdunn20@gmail.com>
2024-06-07 11:53:33 -0700

    changes

---
+++ app/notes/page.tsx
@@ -0,0 +1,11 @@
+import { createClient } from '@/utils/supabase/server';
+
+
+// Example component that fetches data from Supabase
+
+export default async function Notes() {
+  const supabase = createClient();
+  const { data: notes } = await supabase.from("notes").select();
+
+  return <pre>{JSON.stringify(notes, null, 2)}</pre>
+}
