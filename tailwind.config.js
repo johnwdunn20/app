479e6440f5f8ba10a91e67b8886e3d4d39409fa1
John Dunn <johnwdunn20@gmail.com>
2024-06-09 21:26:44 -0700

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,8 @@
 /** @type {import('tailwindcss').Config} */
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
@@ -16,7 +20,21 @@ module.exports = {
       },
     },
   },
+  darkMode: "class",
   plugins: [
-    require('daisyui')
+    require('daisyui'),
+    addVariablesForColors,
   ],
 };
+
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+function addVariablesForColors({ addBase, theme }) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+ 
+  addBase({
+    ":root": newVars,
+  });
+}
