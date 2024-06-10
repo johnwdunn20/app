7b7baccf242efa81e099a8da80a92e2b494353fa
John Dunn <johnwdunn20@gmail.com>
2024-06-09 21:45:12 -0700

    got navbar working

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { createClient } from "@/utils/supabase/server";
 import ConnectSupabaseSteps from "@/components/tutorial/ConnectSupabaseSteps";
 import SignUpUserSteps from "@/components/tutorial/SignUpUserSteps";
 import Header from "@/components/Header";
-import Nav from "@/components/Nav";
+import NavBar from "@/components/Nav";
 
 export default async function Index() {
   const canInitSupabaseClient = () => {
@@ -22,7 +22,7 @@ export default async function Index() {
 
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
-      <Nav/>
+      <NavBar/>
 
       <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
         <Header />
