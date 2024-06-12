c0b780c306742eef1d45a2e44336889639a22e95
John Dunn <johnwdunn20@gmail.com>
2024-06-12 11:27:02 -0700

    update menu

--- components/nav/Nav.tsx
+++ components/nav/Nav.tsx
@@ -74,7 +74,7 @@ export default function Navbar({ className }: { className?: string }) {
   const [active, setActive] = useState<string | null>(null);
   return (
     <div
-      className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
+      className={cn("fixed top-0 inset-x-0 w-full z-50 bg-white shadow-md", className)}
     >
       <Menu setActive={setActive}>
         <MenuItem setActive={setActive} active={active} item="Services">
